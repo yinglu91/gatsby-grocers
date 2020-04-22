87271fda33aa8d89effb0e4879fbe91d586cc57a
Ying Lu <yinglu91@gmail.com>
2020-04-22 18:46:09 -0400

    add categories

--- src/components/Grocers/GrocerList.jsx
+++ src/components/Grocers/GrocerList.jsx
@@ -2,19 +2,60 @@ import React, { useState, useEffect } from 'react'
 import styles from '../../css/items.module.css'
 import Grocer from './Grocer'
 import Title from '../Title'
+import { FaSearch } from 'react-icons/fa'
 
 const GrocerList = props => {
   const [grocers, setGrocers] = useState([])
   const [sortedGrocers, setSortedGrocers] = useState([])
+  const [categories, setCategories] = useState([])
+
+  const getCategories = items => {
+    let tempItems = items.map(item => item.node.category)
+
+    // rm duplicate item and sort them
+    let tempCategories = new Set(tempItems)
+    let categories1 = Array.from(tempCategories)
+    categories1.sort()
+
+    categories1 = ['all', ...categories1]
+
+    return categories1
+  }
 
   useEffect(() => {
     setGrocers(props.grocers.edges)
     setSortedGrocers(props.grocers.edges)
+
+    setCategories(getCategories(props.grocers.edges))
   }, [])
 
+  const handleItem = category => {
+    const tempItems = [...grocers]
+    if (category === 'all') {
+      setSortedGrocers(tempItems)
+    } else {
+      let items = tempItems.filter(({ node }) => node.category === category)
+      setSortedGrocers(items)
+    }
+  }
   return (
     <section className={styles.tours}>
-      <Title title="our" subtitle="grocers" />
+      <Title title="our123" subtitle="grocers" />
+      {/* cateory */}
+
+      <div className={styles.center}>
+        {categories.map(item => (
+          <button
+            className="btn-primary"
+            style={{ width: 170 }}
+            onClick={() => handleItem(item)}
+          >
+            {item}
+          </button>
+        ))}
+        <FaSearch className={styles.icon} />
+      </div>
+
       <div className={styles.center}>
         {sortedGrocers.map(({ node }) => {
           return <Grocer key={node.id} grocer={node} />
